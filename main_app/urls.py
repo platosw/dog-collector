6bed93cc7e90d6063eaa6b05c2cf809fa3a50ef6
Jason Park <platosw83@gmail.com>
2021-11-19 03:15:15 -0500

    First Commit

---
+++ main_app/urls.py
@@ -0,0 +1,13 @@
+from django.urls.resolvers import URLPattern
+
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # we will define all app-level urls in this list
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('dogs/', views.dogs_index, name='index'),
+    path('dogs/<int:dog_id>/', views.dogs_detail, name='detail'),
+]
---
+++ main_app/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Dog
+
+
+
+# Add the Cat class & list and view function below the imports
+# class Dog:  # Note that parens are optional if not inheriting from another class
+#   def __init__(self, name, breed, gender, description, age):
+#     self.name = name
+#     self.breed = breed
+#     self.gender = gender
+#     self.description = description
+#     self.age = age
+
+# dogs = [
+#   Dog('Lolo', 'Jindo', 'F', 'white color', 3),
+#   Dog('Sachi', 'Siberian Huskey', 'M', 'trouble maker', 0),
+#   Dog('Raven', 'Shepherd', 'M', 'friendly', 4)
+# ]
+
+
+# Create your views here.
+def home(request):
+    """
+    this is where we can return a response
+    in most cases we render a template
+    we'll also need some data for that template in most cases
+    """
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def dogs_index(request):
+    dogs = Dog.objects.all()
+    return render(request, 'dogs/index.html', { 'dogs': dogs }) # context object
+
+def dogs_detail(request, dog_id):
+    dog = Dog.objects.get(id=dog_id)
+    return render(request, 'dogs/detail.html', { 'dog': dog })
