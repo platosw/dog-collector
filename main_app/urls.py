3276c073f300e9ed70b30ed03c6651b679d6cdec
Jason Park <platosw83@gmail.com>
2021-11-20 20:08:25 -0500

    just testing..

--- main_app/urls.py
+++ main_app/urls.py
@@ -10,11 +10,13 @@ urlpatterns = [
     # path('dogs/<int:dog_id>/', views.dogs_detail, name='detail'),
     path('dogs/<int:pk>/', views.DogDetail.as_view(), name='detail'),
     path('dogs/create/', views.DogCreate.as_view(), name='dogs_create'),
-    path('dogs/<int:pk>/update', views.DogUpdate.as_view(), name='dogs_update'),
-    path('dogs/<int:pk>/delete', views.DogDelete.as_view(), name='dogs_delete'),
+    path('dogs/<int:pk>/update/', views.DogUpdate.as_view(), name='dogs_update'),
+    path('dogs/<int:pk>/delete/', views.DogDelete.as_view(), name='dogs_delete'),
     path('toys/', views.ToyIndex.as_view(), name='toy_index'),
     path('toys/<int:pk>/', views.ToyDetail.as_view(), name='toy_detail'),
-    path('toys/create', views.ToyCreate.as_view(), name='toy_create'),
-    path('toys/<int:pk>/update', views.ToyUpdate.as_view(), name="toy_update"),
-    path('toys/<int:pk>/delete', views.ToyDelete.as_view(), name="toy_delete"),
+    path('toys/create/', views.ToyCreate.as_view(), name='toy_create'),
+    path('toys/<int:pk>/update/', views.ToyUpdate.as_view(), name="toy_update"),
+    path('toys/<int:pk>/delete/', views.ToyDelete.as_view(), name="toy_delete"),
 ]
+
+# just for testing...
