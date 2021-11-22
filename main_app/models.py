9eb1d1e353115e100a0fdae1094e2f1a2379bd78
Jason Park <platosw83@gmail.com>
2021-11-21 20:51:45 -0500

    finish to update feeding function

--- main_app/models.py
+++ main_app/models.py
@@ -38,6 +38,9 @@ class Feeding(models.Model):
     dog = models.ForeignKey(Dog, on_delete=models.CASCADE)
     def __str__(self):
         return f'{self.get_meal_display()} on {self.date}'
+    
+    class Meta:
+        ordering = ['-date']
 
 
 # Toy model
